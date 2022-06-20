3369f3bce3b2bdc928f21715f4eb023f4b536580
CamModelTools <sven.mitscherlich@i4me.de>
2022-06-20 16:02:49 +0200

    Projektdateien hinzufügen.

---
+++ LovenseWrapper/Extensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LovenseWrapper
+{
+    public static class Extensions
+    {
+        public static string ToyIdStr(this StLovenseToyInfo stLovenseToyInfo)
+        {
+            string toyIdentification = "";
+            unsafe
+            {
+                for (int i = 0; i < 64; i++)
+                {
+                    if ((char)stLovenseToyInfo.ToyId[i] == 0x00)
+                    {
+                        break;
+                    }
+
+                    toyIdentification += (char)stLovenseToyInfo.ToyId[i];
+                }
+            }
+            return toyIdentification;
+        }
+
+        public static string ToyNameStr(this StLovenseToyInfo stLovenseToyInfo)
+        {
+            string toyNameTest = "";
+            unsafe
+            {
+                for (int i = 0; i < 64; i++)
+                {
+                    if ((char)stLovenseToyInfo.ToyName[i] == 0x00)
+                    {
+                        break;
+                    }
+                    toyNameTest += (char)stLovenseToyInfo.ToyName[i];
+                }
+            }
+            return toyNameTest;
+        }
+
+        public static string ToyTypeStr(this StLovenseToyInfo stLovenseToyInfo)
+        {
+            return ((LVSToyType)stLovenseToyInfo.ToyType).ToString();
+        }
+    }
+}
---
+++ LovenseWrapper/LovenseWrapper-symbols.cpp
@@ -0,0 +1,8 @@
+#include <Lovense.h>
+#include <LovenseDef.h>
+#include <LovenseToyInfo.h>
+#include <new>
+
+struct st_lovense_toy_info& (st_lovense_toy_info::*_0)(struct st_lovense_toy_info&&) = &st_lovense_toy_info::operator=;
+extern "C" __declspec(dllexport) void CToyInfo_CToyInfo___1__S_CToyInfo(void* __instance, const CToyInfo& _0) { ::new (__instance) CToyInfo(_0); }
+class CToyInfo& (CToyInfo::*_1)(const class CToyInfo&) = &CToyInfo::operator=;
